3be441f195981ecf2e50279e13c0ef586f602ef6
Nagappan A/L Venkateswaran <96407873+NagappanV@users.noreply.github.com>
2024-05-29 13:31:59 +0800

    The app will now be able to remember what picture it is at. App starts at fist image. When changing rotation, the image doesn't change.

--- app/src/main/java/com/example/lab03animation/MainActivity.kt
+++ app/src/main/java/com/example/lab03animation/MainActivity.kt
@@ -26,9 +26,23 @@ class MainActivity : AppCompatActivity() {
         animationImageView = findViewById(R.id.animationimageView)
         button = findViewById(R.id.button2)
 
+        // Restore the saved instance state (if any)
+        if (savedInstanceState != null) {
+            currentImageIndex = savedInstanceState.getInt("currentImageIndex", 0)
+        }
+
+        // Set the initial image
+        animationImageView.setImageResource(imageIds[currentImageIndex])
+
         button.setOnClickListener {
             currentImageIndex = (currentImageIndex + 1) % imageIds.size
             animationImageView.setImageResource(imageIds[currentImageIndex])
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // Save the current image index
+        outState.putInt("currentImageIndex", currentImageIndex)
     }
 }
